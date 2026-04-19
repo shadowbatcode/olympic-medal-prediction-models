48bc3f51661e678bb1b0d862191ffc6cc4095a2e
Wu <145132885+shadowbatcode@users.noreply.github.com>
2026-04-19 18:49:20 +0800

    Delete problem_c directory

--- problem_c/vison_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = pd.read_csv('data.csv')
-describe = data[['Gold','Silver','Bronze','Total','Rank']].describe()
-
-
-describe.to_csv('describe.csv')
